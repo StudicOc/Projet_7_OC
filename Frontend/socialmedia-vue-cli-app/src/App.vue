59566df8834accbda88c874ae4bae4459e20d98c
StudicOC <sabrinaleb48@gmail.com>
2022-03-08 20:44:35 +0100

    Réorganisation du code

---
+++ Frontend/socialmedia-vue-cli-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/inscription">Inscription</router-link> |
+    </div>
+  </nav>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+nav {
+  background-color: #7888c1;
+}
+</style>
