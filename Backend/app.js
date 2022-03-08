a650374a55c2367951472000c24c785819a4a05a
StudicOC <sabrinaleb48@gmail.com>
2022-03-08 20:47:16 +0100

    Réorganisation du code

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,36 @@ const express = require("express");
 
 const app = express();
 
+const mysql = require("mysql");
+//---Variables env---//
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+//---Sécurisation---//
+
+const helmet = require("helmet");
+app.use(helmet());
+
+app.disable("x-powered-by");
+
+// ------------ //
+
+// ---Connect to MySql--- //
+
+const db = mysql.createConnection({
+  host: "localhost",
+
+  user: "root",
+
+  password: "133qlop@",
+});
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+});
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
