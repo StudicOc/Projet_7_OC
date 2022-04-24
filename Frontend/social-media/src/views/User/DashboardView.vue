bffdd34abb01697c19fd71106a9600c69a7ad7ee
StudicOC <sabrinaleb48@gmail.com>
2022-04-24 15:53:12 +0200

    Séparation des pages public et user

---
+++ Frontend/social-media/src/views/User/DashboardView.vue
@@ -0,0 +1,13 @@
+<template>
+  <ProfilConnect />
+</template>
+
+<script>
+import ProfilConnect from "../../components/ProfilConnect.vue";
+
+export default {
+  components: {
+    ProfilConnect,
+  },
+};
+</script>
