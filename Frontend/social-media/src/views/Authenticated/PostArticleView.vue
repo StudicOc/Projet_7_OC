69e4d295710473c4e38d7dfd45dc0a88878d7f3a
StudicOC <sabrinaleb48@gmail.com>
2022-05-21 16:01:16 +0200

    Réfactorisation du code et gestion des dossiers

---
+++ Frontend/social-media/src/views/Authenticated/PostArticleView.vue
@@ -0,0 +1,13 @@
+<template>
+  <PostArticle />
+</template>
+
+<script>
+import PostArticle from "../../components/PostArticle.vue";
+
+export default {
+  components: {
+    PostArticle,
+  },
+};
+</script>
