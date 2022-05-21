b287a46f2404bad40fedda0ee5d47b88ae13ebcf
StudicOC <sabrinaleb48@gmail.com>
2022-05-21 16:44:04 +0200

    Suppression du css inutile

--- Frontend/social-media/src/App.vue
+++ Frontend/social-media/src/App.vue
@@ -1,94 +1,99 @@
-<template>
-  <div id="app">
-    <NavBar />
-  </div>
+<!-- ./src/App.vue -->
 
-  <!--Tous les composants auront accés à l'user ( token )-->
+<template>
+  <!-- Header -->
+  <div id="app">
+    <div class="top-bar">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav" v-if="isLoggedIn">
+              <!--Lien de de notre nav relié au routeur-->
+              <router-link v-if="isLoggedIn" to="/profil"
+                >Mon espace</router-link
+              >
+              <router-link v-if="isLoggedIn" to="/article/add"
+                >Poster un article</router-link
+              >
+
+              <router-link v-if="isLoggedIn" to="/articles"
+                >Article</router-link
+              >
+              <a href="#" v-if="isLoggedIn" class="nav-link" @click="logout"
+                >Logout</a
+              >
+            </div>
+            <span v-else>
+              <!--Lien de de notre nav relié au routeur-->
+              <div class="navbar-nav">
+                <router-link to="/">Groupomania</router-link>
+                <router-link to="/signup">Inscription</router-link>
+                <router-link to="/login">Login</router-link>
+              </div>
+            </span>
+          </div>
+        </div>
+      </nav>
+
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
 export default {
   name: "App",
-  components: {
-    NavBar,
+  // Propriété calculée //
+  computed: {
+    isLoggedIn: function () {
+      return this.$store.getters.isLoggedIn;
+    },
+  },
+
+  methods: {
+    logout: function () {
+      this.$store.dispatch("logout").then(() => {
+        this.$router.push("/");
+      });
+    },
   },
 };
 </script>
 
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Lato&display=swap");
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+html {
+  background-color: #f1f1f1;
+}
 #app {
-  font-family: "Lato", sans-serif;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  margin: 0;
 }
+
 nav {
-  padding: 30px;
+  background-color: #2c3e50;
+  color: white;
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    font-size: 15px;
     text-decoration: none;
-    text-transform: uppercase;
     padding: 10px;
     font-size: 14px;
-    &.router-link-exact-active {
-      color: white;
-      background: rgb(72, 14, 86);
-      border-radius: 4px;
-    }
-  }
-}
-/*Animation loading spinner*/
-.loader-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1000;
-  background-color: white;
-  /*animation-fill-mode:forwards;
-  */ /*animation-delay:3s;
-  */ /*animation-name:;
-  hidden*/
-  animation: hidden 0.5s linear 1 forwards 3s;
-  -moz-animation: hidden 0.5s linear 1 forwards 3s;
-  -webkit-animation: hidden 0.5s linear 1 forwards 3s;
-  -o-animation: hidden 0.5s linear 1 forwards 3s;
-}
-.loader {
-  width: 120px;
-  height: 120px;
-  border: 6px solid #c5582e;
-  border-radius: 50%;
-  border-top-color: #3c969c;
-  border-bottom-color: #c5582e;
-  border-left-color: rgb(72, 14, 86);
-  animation: spin 0.3s infinite;
-  -moz-animation: spin 0.3s infinite;
-  -webkit-animation: spin 0.3s infinite;
-  -o-animation: spin 0.3s infinite;
-}
-@keyframes hidden {
-  100% {
-    opacity: 0; /*transparence des éléments*/
-    z-index: -1;
-  }
-}
-@keyframes spin {
-  100% {
-    transform: rotate(360deg);
-    -webkit-transform: rotate(360deg);
-    -moz-transform: rotate(360deg);
-    -ms-transform: rotate(360deg);
-    -o-transform: rotate(360deg);
+    color: inherit;
   }
 }
 </style>
