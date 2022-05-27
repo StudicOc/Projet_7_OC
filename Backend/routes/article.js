00983d780a828c2c1fd630e642e5c9353cb2fd21
StudicOC <sabrinaleb48@gmail.com>
2022-05-27 16:33:22 +0200

    Modification de la logique des routes

--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const articleCtrl = require("../controllers/article");
 const auth = require("../middleware/auth");
 
-//*Création d'un article */
+const articleCtrl = require("../controllers/article");
+//****Création d'un article ****//
 router.post("/add", auth, articleCtrl.createArticle);
 
-//*Obtenir tous un article */
+//*****Obtenir un article*****//
 router.get("/:id", auth, articleCtrl.getOneArticle);
 
-//*Mettre à jour l'article par identifiant */
+//********Mettre à jour l'article par son id*********//
 router.put("/:id", auth, articleCtrl.modifyArticle);
 
-//*Supprimer l'article par identifiant */
+//*******Supprimer l'article par son id **********//
 router.delete("/:id", auth, articleCtrl.deleteArticle);
 
 module.exports = router;
