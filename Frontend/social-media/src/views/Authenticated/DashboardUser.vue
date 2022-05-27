aec77c9eee1ee31cdd86b12a4b039f1444a1fe77
StudicOC <sabrinaleb48@gmail.com>
2022-05-27 16:32:45 +0200

    Page Dashboard after login - Profil user

---
+++ Frontend/social-media/src/views/Authenticated/DashboardUser.vue
@@ -0,0 +1,14 @@
+<template>
+  <ProfilConnect />
+</template>
+
+<script>
+import ProfilConnect from "../../components/Profile-Information.vue";
+
+export default {
+  name: "DashboardUser",
+  components: {
+    ProfilConnect,
+  },
+};
+</script>
