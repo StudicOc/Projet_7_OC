fa8726f51903d6dcfdfec65a158182feaa174f18
StudicOC <sabrinaleb48@gmail.com>
2022-05-30 14:01:26 +0200

    Ajout d'une sécurisation avec les regex

--- Frontend/social-media/src/views/Public-page/Signup.vue
+++ Frontend/social-media/src/views/Public-page/Signup.vue
@@ -5,20 +5,44 @@
       class="d-flex flex-column justify-content-center align-items-center mt-5"
     >
       <div class="form-group col-lg-5 pt-5">
-        <input type="text" placeholder="Prénom" v-model="firstname" />
+        <input
+          type="text"
+          placeholder="Prénom"
+          v-model="firstname"
+          aria-label="Saisir votre prénom"
+          required
+        />
+        <span v-if="msg.firstname">{{ msg.firstname }}</span>
       </div>
       <div class="form-group col-lg-5 pt-5">
-        <input type="text" placeholder="Nom de famille" v-model="lastname" />
+        <input
+          type="text"
+          placeholder="Nom de famille"
+          v-model="lastname"
+          aria-label="Saisir votre nom"
+          required
+        />
+        <span v-if="msg.lastname">{{ msg.lastname }}</span>
       </div>
       <div class="form-group col-lg-5 pt-5">
-        <input type="text" placeholder="Email" v-model="email" />
+        <input
+          type="text"
+          placeholder="Email"
+          v-model="email"
+          aria-label="Saisir votre mail"
+          required
+        />
+        <span v-if="msg.email">{{ msg.email }}</span>
       </div>
       <div class="form-group col-lg-5 pt-5">
         <input
           type="password"
           placeholder="Mot de passe"
           v-model="password_key"
+          aria-label="Saisir votre mot de passe"
+          required
         />
+        <span v-if="msg.password_key">{{ msg.password_key }}</span>
       </div>
 
       <div class="form-group col-lg-5 text-center pt-5">
@@ -41,9 +65,64 @@ export default {
       email: "",
       password_key: "",
       isAdmin: "",
+      msg: [],
     };
   },
+  watch: {
+    firstname() {
+      this.validateFirstname(this.firstname);
+    },
+    lastname() {
+      this.validateLastname(this.lastname);
+    },
+    email() {
+      this.validateEmail(this.email);
+    },
+    password_key() {
+      this.validatePassword_key(this.password_key);
+    },
+  },
   methods: {
+    validateFirstname(firstname) {
+      const regexName = /([A-Z])\w+/g;
+      if (regexName.test(firstname)) {
+        this.msg["firstname"] = "Format accepté";
+      } else {
+        this.msg["firstname"] =
+          "Votre Prénom doit contenir une lettre majuscule et au moins 2 lettres ";
+      }
+    },
+
+    validateLastname(lastname) {
+      //const regexName = /^[a-zA-Z-\s]+$/;
+      const regexName = /([A-Z])\w+/g;
+      if (regexName.test(lastname)) {
+        this.msg["lastname"] = "Format accepté";
+      } else {
+        this.msg["lastname"] =
+          "Votre Prénom doit contenir une lettre majuscule et au moins 2 lettres";
+      }
+    },
+
+    validateEmail(email) {
+      if (/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        this.msg["email"] = "Mail valide";
+      } else {
+        this.msg["email"] = "Le format mail n'est pas respecté";
+      }
+    },
+    validatePassword_key(password_key) {
+      let difference = 8 - password_key.length;
+      if (password_key.length < 8) {
+        this.msg["password_key"] =
+          "Doit contenir 8 caractères! " +
+          difference +
+          " 8 caractères restants";
+      } else {
+        this.msg["password_key"] = "Mot de passe valide";
+      }
+    },
+
     submitForm() {
       const dataUser = {
         firstname: this.firstname,
@@ -52,15 +131,18 @@ export default {
         password_key: this.password_key,
         isAdmin: this.isAdmin,
       };
-      console.log(dataUser);
+      //console.log(dataUser);
       axios
         .post("http://localhost:3000/api/auth/signup", dataUser)
         .then((response) => {
+          this.data = response.data;
           console.log(response.data);
           this.$router.push("/login");
         })
         .catch((error) => {
-          alert("Un utilisateur est déjà inscrit avec cette mail");
+          alert(
+            "Vérifier les informations saisies du formulaire d'inscription"
+          );
           console.log(error);
         });
     },
