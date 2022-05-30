99786b0fd3ec501ef497971b754c3162deb8e95b
StudicOC <sabrinaleb48@gmail.com>
2022-05-30 16:45:36 +0200

    gestion de l'affichage des dates

---
+++ Frontend/social-media/src/Service/formatDateMixin.js
@@ -0,0 +1,11 @@
+export default {
+  methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString);
+      // Then specify how you want your dates to be formatted
+      return new Intl.DateTimeFormat("default", { dateStyle: "long" }).format(
+        date
+      );
+    },
+  },
+};
