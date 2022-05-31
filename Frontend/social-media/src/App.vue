f49cf798c830f4ad65b80fccb295de58cc04a29f
StudicOC <sabrinaleb48@gmail.com>
2022-05-31 14:29:53 +0200

    Responsive design

--- Frontend/social-media/src/App.vue
+++ Frontend/social-media/src/App.vue
@@ -4,50 +4,48 @@
   <!-- Header -->
   <div id="app">
     <div class="top-bar">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="navbar-nav" v-if="isLoggedIn">
-              <!--Lien de de notre nav relié au routeur-->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Groupomania</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="navbar-nav" v-if="isLoggedIn">
+            <!--Lien de de notre nav relié au routeur-->
 
-              <router-link v-if="isLoggedIn" to="/profil">
-                <font-awesome-icon icon="user" /> Compte
-              </router-link>
+            <router-link v-if="isLoggedIn" to="/profil">
+              <font-awesome-icon icon="user" /> Compte
+            </router-link>
 
-              <!--<router-link v-if="isLoggedIn" to="/article/add"
+            <!--<router-link v-if="isLoggedIn" to="/article/add"
                 >Poster un article</router-link
               -->
 
-              <router-link v-if="isLoggedIn" to="/articles"
-                >Workplace</router-link
-              >
-              <a href="#" v-if="isLoggedIn" class="nav-link" @click="logout"
-                >Logout</a
-              >
-            </div>
-            <span v-else>
-              <!--Lien de de notre nav relié au routeur-->
-              <div class="navbar-nav">
-                <router-link to="/">Groupomania</router-link>
-                <router-link to="/signup">Inscription</router-link>
-                <router-link to="/login">Login</router-link>
-              </div>
-            </span>
+            <router-link v-if="isLoggedIn" to="/articles"
+              >Workplace</router-link
+            >
+            <a href="#" v-if="isLoggedIn" class="nav-link" @click="logout"
+              >Logout</a
+            >
           </div>
+          <span v-else>
+            <!--Lien de de notre nav relié au routeur-->
+            <div class="navbar-nav">
+              <router-link to="/">FAQ</router-link>
+              <router-link to="/signup">Inscription</router-link>
+              <router-link to="/login">Login</router-link>
+            </div>
+          </span>
         </div>
       </nav>
-
       <router-view />
     </div>
   </div>
@@ -92,14 +90,15 @@ html {
 }
 
 nav {
-  background-color: $color-tertiary;
-  color: white;
+  //background-color: $color-tertiary;
+  color: black;
   a {
     font-size: 15px;
     text-decoration: none;
     padding: 10px;
     font-size: 14px;
     color: inherit;
+    font-weight: bold;
   }
 }
 </style>
