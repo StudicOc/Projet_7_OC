132de24f3891ad2ea2bc0fb87065a6aece209913
StudicOC <sabrinaleb48@gmail.com>
2022-06-10 20:18:39 +0200

    Logo component

---
+++ Frontend/social-media/src/components/BrandSocialMedia.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center img">
+    <img alt="Logo de groupomania" src="../assets/icon-above.webp" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BrandSocialMedia",
+};
+</script>
+
+<style>
+img {
+  max-width: 40%;
+  height: auto;
+}
+@media only screen and (max-width: 800px) {
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+}
+</style>
