832ff29fb8986bd5a7b20fe3c360ad5938cccb48
StudicOC <sabrinaleb48@gmail.com>
2022-06-13 15:39:33 +0200

    Ajout du comoosant pour un article :id

--- Frontend/social-media/src/views/Authenticated/ArticlePage.vue
+++ Frontend/social-media/src/views/Authenticated/ArticlePage.vue
@@ -1,20 +1,8 @@
 <template>
-  <div class="mt-4 site">
-    <nav class="side-bar mx-4">
-      <a href="#">Fil d'actualité</a>
-      <a href="#">Messages</a>
-      <a href="#">Réunions</a>
-      <a href="#"> <font-awesome-icon icon="coffee" />Évènement </a>
-      <a href="#">Groupes</a>
-      <a href="#">Amis</a>
-    </nav>
+  <div class="mt-4">
     <main>
       <IdArticle />
     </main>
-    <aside class="aside">
-      <p class="font-weight-bold">Groupes suggérés</p>
-      <p>Découvrez et rejoignez des groupes dont vos collègues font partie.</p>
-    </aside>
   </div>
 </template>
 
