da95de198a07f56264690f31fcd2414fc87a8e65
StudicOC <sabrinaleb48@gmail.com>
2022-06-13 15:32:48 +0200

    include findAll

--- Backend/controllers/articles.js
+++ Backend/controllers/articles.js
@@ -1,11 +1,9 @@
 const Article = require("../models/Article");
-const Comment = require("../models/articles_comments");
+//const Comment = require("../models/Comment");
 
-//******* Récupérer tous les articles de la BDD *******//
+//*******************Obtenez tous les articles avec des commentaires****************** */
 exports.getAllArticle = (req, res, next) => {
-  Article.findAll({
-    order: [["createdAt", "DESC"]],
-  })
+  Article.findAll({ order: [["createdAt", "DESC"]] })
     .then((articles) => {
       res.status(200).json(articles);
     })
@@ -15,27 +13,3 @@ exports.getAllArticle = (req, res, next) => {
       });
     });
 };
-
-exports.deleteArticle = (req, res, next) => {
-  // Condition par défaut //
-  let whereClause = { _id: req.params.id, userId: req.userId };
-  // Condition pour un Admin //
-  if (req.isAdmin > 0) {
-    delete whereClause.userId;
-  }
-  console.log(whereClause);
-  Article.destroy({ where: whereClause })
-
-    .then((destroyedRows) => {
-      if (destroyedRows) {
-        // Si plus d'une ligne //
-        res.status(200).json({ message: "Objet supprimé!" });
-      } else {
-        res.status(400).json({ message: " Object non supprimé!" });
-      }
-      //console.log(test[0]);
-    })
-    .catch((error) => res.status(404).json({ error }));
-};
-
-//****************************Ajouter un commentaire à un article**************************//
