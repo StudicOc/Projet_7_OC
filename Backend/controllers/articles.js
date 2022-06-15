71268304eded7e55d0e815b9cdf5ae8682e63569
StudicOC <sabrinaleb48@gmail.com>
2022-06-15 16:34:24 +0200

    SQL

--- Backend/controllers/articles.js
+++ Backend/controllers/articles.js
@@ -1,15 +1,19 @@
-const Article = require("../models/Article");
-//const Comment = require("../models/Comment");
+const db = require("../config.db/db");
 
 //*******************Obtenez tous les articles avec des commentaires****************** */
-exports.getAllArticle = (req, res, next) => {
-  Article.findAll({ order: [["createdAt", "DESC"]] })
-    .then((articles) => {
-      res.status(200).json(articles);
-    })
-    .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
-    });
+exports.getAllArticle = async (req, res) => {
+  try {
+    const articleComment = await db.query(
+      "SELECT * FROM articles left join comments on comments.ArticleId = articles._id",
+      (error, results, fields) => {
+        if (error) {
+          res.json({ error });
+        } else {
+          res.status(200).json({ results, articleComment });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
